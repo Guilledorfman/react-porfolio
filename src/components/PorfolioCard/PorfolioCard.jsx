455afda21d32a7020e632d103e17b767cd349c4b
Guille <guillermodorfman@gmail.com>
2022-02-14 00:51:15 -0300

    media queries porfolio

--- src/components/PorfolioCard/PorfolioCard.jsx
+++ src/components/PorfolioCard/PorfolioCard.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import CodeIcon from '@mui/icons-material/Code';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 
-const PorfolioCard = ({name, img, subtitle, text, cajaClass, index, setActiveNumber}) => {
+const PorfolioCard = ({name, img, subtitle, text, cajaClass, index, setActiveNumber, tech}) => {
 
 
 const [ readMoreActive, setReadMoreActive ] = useState(false)
@@ -10,6 +10,15 @@ const [ readMoreActive, setReadMoreActive ] = useState(false)
 useEffect(()=>{
     cajaClass === 'caja' ? setReadMoreActive(false) : <></>
 },[cajaClass])
+
+
+const techList = tech.map((e, index)=>{
+    return(
+        <li key={index}>
+            {e}
+        </li>
+    )
+})
 
   return (
         <div className={cajaClass} onClick={()=>{setActiveNumber(index)}} style={{ backgroundImage: `url(${img})` }}>
@@ -19,6 +28,11 @@ useEffect(()=>{
                 </p>
             </div>
             <h2 className="nombre">{name}</h2>
+                <ul>
+                    <b>Technology used:</b>
+                   {techList}
+                </ul>
+
             <div className={'readmore ' + (readMoreActive && 'active')}>
                 <div className="button" onClick={()=>{setReadMoreActive(!readMoreActive)}}>
                     <div className="readmore-bars">
@@ -26,7 +40,6 @@ useEffect(()=>{
                         <div className="bar2"></div>
                     </div>
                     {readMoreActive ? <i>close</i> : <i>read more</i>}
-                    
                 </div>
             </div>
             <div className="buttons">
